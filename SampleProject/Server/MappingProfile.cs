203255842faaa9621fd4213dc65591779945fb5c
Ehsan Nozari <visualstadio@gmail.com>
2023-05-28 11:03:16 +0330

    map models

---
+++ SampleProject/Server/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using SampleProject.Server.Data;
+using SampleProject.Server.VModels;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        // Add as many of these lines as you need to map your objects
+        CreateMap<Product, ProductModel>().ReverseMap();
+    }
+}
