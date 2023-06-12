da45fefa9e3c720ee3a603068d6be22f892d4d11
Ehsan Nozari <visualstadio@gmail.com>
2023-06-12 15:08:11 +0330

    mappingRelated

--- SampleProject/Server/MappingProfile.cs
+++ SampleProject/Server/MappingProfile.cs
@@ -4,5 +4,6 @@
     {
         // Add as many of these lines as you need to map your objects
         CreateMap<Product, ProductModel>().ReverseMap();
+        CreateMap<RelatedProduct, RelatedProductModel>().ReverseMap();
     }
 }
