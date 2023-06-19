45a0bbf25ebbc07d8d7613855abff99d4f598661
Ehsan Nozari <visualstadio@gmail.com>
2023-06-19 16:25:24 +0330

    new model method

--- SampleProject/Shared/Dtos/RelatedProductDto.cs
+++ SampleProject/Shared/Dtos/RelatedProductDto.cs
@@ -7,21 +7,8 @@ namespace SampleProject.Shared.Dtos
     public class RelatedProductDto
     {
         public int ProductId1 { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [ForeignKey(nameof(Product2))]
         public int ProductId2 { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>   
         public ProductDto Product2 { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
         public int DisplayOrder { get; set; }
     }
 }
