c3b88470c44751f06f450338d9d1cc15316038c8
Ehsan Nozari <visualstadio@gmail.com>
2023-08-09 08:43:33 +0330

    newClient

---
+++ SampleProject/NewClient/Pages/Product/Edit.razor.cs
@@ -0,0 +1,44 @@
+using global::System;
+using global::System.Collections.Generic;
+using global::System.Linq;
+using global::System.Threading.Tasks;
+using global::Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
+using Microsoft.JSInterop;
+using MudBlazor;
+using SampleProject.Shared.Dtos;
+using SampleProjects.Shared.ViewModels.Product;
+
+namespace SampleProject.NewClient.Pages.Product
+{
+    public partial class Edit
+    {
+        private ProductVM? productVM = new();
+        [Parameter]
+        public int Id { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            productVM = await _httpClient.GetFromJsonAsync<ProductVM>($"api/Product/EditAsync/{Id}");
+        }
+
+        public async Task EditData()
+        {
+            var responce = await _httpClient.PostAsJsonAsync("api/Product/EditAsync", productVM);
+            if (responce.IsSuccessStatusCode)
+                _navigationManager.NavigateTo("Product/Index");
+        }
+
+        private void BackToList()
+        {
+            _navigationManager.NavigateTo("Product/Index");
+        }
+    }
+}
---
+++ SampleProject/NewClient/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor;
+using MudBlazor.Services;
+using SampleProject.NewClient;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("SampleProject.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("SampleProject.ServerAPI"));
+builder.Services.AddScoped<IDialogService, DialogService>();
+builder.Services.AddApiAuthorization();
+builder.Services.AddMudServices();
+
+await builder.Build().RunAsync();
