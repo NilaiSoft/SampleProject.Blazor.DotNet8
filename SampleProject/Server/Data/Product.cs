cb16741e9a43501a507667790f15dffe7c32ce57
Ehsan Nozari <visualstadio@gmail.com>
2023-11-09 08:58:51 +0330

    published property in peroduct

--- SampleProject/Server/Data/Product.cs
+++ SampleProject/Server/Data/Product.cs
@@ -1,52 +1,57 @@
-﻿namespace SampleProject.Server.Data
+namespace SampleProject.Server.Data
 {
-    public partial class Product : BaseEntity, ISoftDeletedEntity
+  public partial class Product : BaseEntity, ISoftDeletedEntity
+  {
+    public Product()
     {
-        public Product()
-        {
-            Name = string.Empty;
-            ShortDescription = string.Empty;
-            FullDescription = string.Empty;
-            AdminComment = string.Empty;
-            ShowOnHomepage = true;
-            Enable = true;
-            Deleted = false;
-        }
-
-        public string Name { get; set; }
-
-        /// <summary>
-        /// Gets or sets the short description
-        /// </summary>
-        public string ShortDescription { get; set; }
-
-        /// <summary>
-        /// Gets or sets the full description
-        /// </summary>
-        public string FullDescription { get; set; }
-
-        /// <summary>
-        /// Gets or sets the admin comment
-        /// </summary>
-        public string AdminComment { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public bool ShowOnHomepage { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public bool Enable { get; set; }
-        /// <summary>
-        /// 
-        /// </summary>
-        public bool Deleted { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public virtual ICollection<RelatedProduct> RelatedProducts { get; set; }
+      Name = string.Empty;
+      ShortDescription = string.Empty;
+      FullDescription = string.Empty;
+      AdminComment = string.Empty;
+      ShowOnHomepage = true;
+      Enable = true;
+      Deleted = false;
     }
+
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the short description
+    /// </summary>
+    public string ShortDescription { get; set; }
+
+    /// <summary>
+    /// Gets or sets the full description
+    /// </summary>
+    public string FullDescription { get; set; }
+
+    /// <summary>
+    /// Gets or sets the admin comment
+    /// </summary>
+    public string AdminComment { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public bool ShowOnHomepage { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public bool Enable { get; set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public bool Deleted { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public bool Published { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public virtual ICollection<RelatedProduct> RelatedProducts { get; set; }
+  }
 }
--- SampleProject/Server/VModels/ProductModel.cs
+++ SampleProject/Server/VModels/ProductModel.cs
@@ -1,24 +1,25 @@
-﻿namespace SampleProject.Server.VModels;
+namespace SampleProject.Server.VModels;
 public class ProductModel : BaseModel
 {
-    public ProductModel()
-    {
-        Name = string.Empty;
-        ShortDescription = string.Empty;
-        FullDescription = string.Empty;
-        AdminComment = string.Empty;
-        ShowOnHomepage = true;
-        Enable = true;
-        Deleted = false;
-    }
+  public ProductModel()
+  {
+    Name = string.Empty;
+    ShortDescription = string.Empty;
+    FullDescription = string.Empty;
+    AdminComment = string.Empty;
+    ShowOnHomepage = true;
+    Enable = true;
+    Deleted = false;
+  }
 
-    public string Name { get; set; }
-    public string ShortDescription { get; set; }
-    public string FullDescription { get; set; }
-    public string AdminComment { get; set; }
-    public bool ShowOnHomepage { get; set; }
-    public bool Enable { get; set; }
-    public bool Deleted { get; set; }
-    public int Position { get; set; }
-    //public ICollection<RelatedProduct>? RelatedProducts { get; set; }
+  public string Name { get; set; }
+  public string ShortDescription { get; set; }
+  public string FullDescription { get; set; }
+  public string AdminComment { get; set; }
+  public bool ShowOnHomepage { get; set; }
+  public bool Enable { get; set; }
+  public bool Deleted { get; set; }
+  public int Position { get; set; }
+  public bool Published { get; set; }
+  //public ICollection<RelatedProduct>? RelatedProducts { get; set; }
 }
