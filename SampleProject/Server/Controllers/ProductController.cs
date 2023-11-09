88d9bb1fd9f09f8634c299f42412b6be8fb5f537
Ehsan Nozari <visualstadio@gmail.com>
2023-11-09 09:57:09 +0330

    total count

--- SampleProject/Server/Controllers/ProductController.cs
+++ SampleProject/Server/Controllers/ProductController.cs
@@ -7,107 +7,109 @@ namespace SampleProject.Server.Controllers;
 //[Authorize]
 public class ProductController : BaseController<Product, ProductModel>
 {
-    private readonly IProductService _productService;
-    private readonly IRelatedProductService _relatedProductService;
-    private readonly IMapper _mapper;
+  private readonly IProductService _productService;
+  private readonly IRelatedProductService _relatedProductService;
+  private readonly IMapper _mapper;
 
-    public ProductController(IEntityRepository<Product, ProductModel> repository
-        , IMapper mapper, IProductService productService
-        , IRelatedProductService relatedProductService
-        , ICacheManager<Product> memoryCache)
-        : base(repository, mapper, memoryCache)
+  public ProductController(IEntityRepository<Product, ProductModel> repository
+      , IMapper mapper, IProductService productService
+      , IRelatedProductService relatedProductService
+      , ICacheManager<Product> memoryCache)
+      : base(repository, mapper, memoryCache)
+  {
+    _productService = productService;
+    _relatedProductService = relatedProductService;
+    _mapper = mapper;
+  }
+
+  [HttpGet]
+  [Route($"{nameof(Index)}/{{pageIndex}}/{{pageSize}}")]
+  public override async Task<IActionResult> Index(int pageIndex = 0, int pageSize = int.MaxValue)
+  {
+    var res = await _productService.GetAllAsync(pageIndex, pageSize == 0 ? 10 : pageSize);
+
+    var result = _mapper.Map<IList<ProductModel>>(res);
+
+    result = result.OrderBy(x => x.Name).ToList();
+    foreach (var item in result)
     {
-        _productService = productService;
-        _relatedProductService = relatedProductService;
-        _mapper = mapper;
+      item.Position = result.IndexOf(item) + 1;
+
     }
 
-    [HttpGet]
-    [Route($"{nameof(Index)}/{{pageIndex}}/{{pageSize}}")]
-    public override async Task<IActionResult> Index(int pageIndex = 0, int pageSize = int.MaxValue)
+    var model = new PageOptions<ProductModel>
     {
-        var result = _mapper.Map<IList<ProductModel>>(await _productService.GetAllAsync(pageIndex, pageSize == 0 ? 10 : pageSize));
+      Models = result,
+      PageSize = res.TotalCount,
+      PageIndex = 0
+    };
 
-        result = result.OrderBy(x => x.Name).ToList();
-        foreach (var item in result)
+    return Ok(model);
+  }
+
+  [HttpGet]
+  [Route($"{nameof(SearchProduct)}/{{searchString}}")]
+  public virtual async Task<IActionResult> SearchProduct(string searchString)
+  {
+    var model = await _productService.GetAllAsync(
+        x => (x.Name.Contains(searchString)
+        || x.ShortDescription.Contains(searchString)
+        || x.FullDescription.Contains(searchString)));
+
+    return Ok(model);
+  }
+
+  [HttpGet]
+  [Route($"{nameof(RelatedProducts)}/{{productId}}/{{pageIndex}}/{{pageSize}}")]
+  public virtual async Task<IActionResult> RelatedProducts(int productId = 0, int pageIndex = 0, int pageSize = int.MaxValue)
+  {
+    var model = await _cacheManager.GetAsync($"{typeof(RelatedProduct).Name.ToLower()}-{productId}-List-index-{pageIndex}-{pageSize}"
+        , async Task<IPagedList<RelatedProduct>> () =>
         {
-            item.Position = result.IndexOf(item) + 1;
+          return await _relatedProductService.GetAllAsync(x => x.ProductId1 == productId
+              , x => new RelatedProduct
+              {
+                ProductId1 = x.ProductId1,
+                ProductId2 = x.ProductId2,
+                Product2 = x.Product2,
+                Id = x.Id,
+                DisplayOrder = x.DisplayOrder
+              }, pageIndex, pageSize);
+        });
 
-        }
+    return Ok(new Tuple<IPagedList<RelatedProduct>, int>(model, model.TotalCount));
+  }
 
-        var model = new PageOptions<ProductModel>
-        {
-            Models = result,
-            PageSize = 15,
-            PageIndex = 0
-        };
+  [HttpGet]
+  [Route($"{nameof(EditAsync)}/{{id}}")]
+  public override async Task<IActionResult> EditAsync(int id)
+  {
+    var model = await _productService.FindAsync(id);
 
-        return Ok(model);
-    }
+    return Ok(model);
+  }
 
-    [HttpGet]
-    [Route($"{nameof(SearchProduct)}/{{searchString}}")]
-    public virtual async Task<IActionResult> SearchProduct(string searchString)
-    {
-        var model = await _productService.GetAllAsync(
-            x => (x.Name.Contains(searchString)
-            || x.ShortDescription.Contains(searchString)
-            || x.FullDescription.Contains(searchString)));
+  [HttpPost]
+  [Route(nameof(RelatedCreate))]
+  public virtual async Task<IActionResult> RelatedCreate(IList<RelatedProductModel> entitys)
+  {
+    var product = _mapper.Map<IList<RelatedProduct>>(entitys);
 
-        return Ok(model);
-    }
+    var result = await _relatedProductService.AddRangeAndSaveChangesAsync(product);
 
-    [HttpGet]
-    [Route($"{nameof(RelatedProducts)}/{{productId}}/{{pageIndex}}/{{pageSize}}")]
-    public virtual async Task<IActionResult> RelatedProducts(int productId = 0, int pageIndex = 0, int pageSize = int.MaxValue)
-    {
-        var model = await _cacheManager.GetAsync($"{typeof(RelatedProduct).Name.ToLower()}-{productId}-List-index-{pageIndex}-{pageSize}"
-            , async Task<IPagedList<RelatedProduct>> () =>
-            {
-                return await _relatedProductService.GetAllAsync(x => x.ProductId1 == productId
-                , x => new RelatedProduct
-                {
-                    ProductId1 = x.ProductId1,
-                    ProductId2 = x.ProductId2,
-                    Product2 = x.Product2,
-                    Id = x.Id,
-                    DisplayOrder = x.DisplayOrder
-                }, pageIndex, pageSize);
-            });
+    _cacheManager.RemoveRangeByPrefixEntityName(typeof(RelatedProduct).Name.ToLower());
 
-        return Ok(new Tuple<IPagedList<RelatedProduct>, int>(model, model.TotalCount));
-    }
+    return Ok(result);
+  }
 
-    [HttpGet]
-    [Route($"{nameof(EditAsync)}/{{id}}")]
-    public override async Task<IActionResult> EditAsync(int id)
-    {
-        var model = await _productService.FindAsync(id);
+  [HttpDelete($"{nameof(DeleteRelatedProductAsync)}/{{id}}")]
+  public async Task<IActionResult> DeleteRelatedProductAsync(int id)
+  {
+    var result = await _relatedProductService.DeleteAsync(x => x.Id == id);
 
-        return Ok(model);
-    }
+    _cacheManager.RemoveRangeByPrefixEntityName(typeof(RelatedProduct).Name.ToLower());
 
-    [HttpPost]
-    [Route(nameof(RelatedCreate))]
-    public virtual async Task<IActionResult> RelatedCreate(IList<RelatedProductModel> entitys)
-    {
-        var product = _mapper.Map<IList<RelatedProduct>>(entitys);
-
-        var result = await _relatedProductService.AddRangeAndSaveChangesAsync(product);
-
-        _cacheManager.RemoveRangeByPrefixEntityName(typeof(RelatedProduct).Name.ToLower());
-
-        return Ok(result);
-    }
-
-    [HttpDelete($"{nameof(DeleteRelatedProductAsync)}/{{id}}")]
-    public async Task<IActionResult> DeleteRelatedProductAsync(int id)
-    {
-        var result = await _relatedProductService.DeleteAsync(x => x.Id == id);
-
-        _cacheManager.RemoveRangeByPrefixEntityName(typeof(RelatedProduct).Name.ToLower());
-
-        return Ok(result);
-    }
+    return Ok(result);
+  }
 }
 
